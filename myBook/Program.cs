1dd75e2bb9b331e9a313ca36ec1bfef8acdb274c
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-19 21:45:04 +0900

    0519 실습 일지 04번 문제

--- myBook/Program.cs
+++ myBook/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace myBook
 {
+    class myBook
+    {
+        public string name;
+        public string publish;
+        public string writer;
+        public string publisher;
+        public string publishingCompany;
+        public string chief;
+        public string planner;
+        public string editor;
+        public string designer;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            myBook mybook = new myBook() { name = "PHP 프로그래밍 입문", publish = "2013년 5월 20일", writer = "황재호", publisher = "김태헌", publishingCompany = "한빛아카데미(주)", chief = "김현용", planner = "김이화", editor = "김이화", designer = "여동일" };
         }
     }
 }
